1fa23b303113f6869b743c547dd97cd2d432fe54
Joao Pereira <joaopereira_19@hotmail.com>
2016-03-25 12:55:50 +0000

    update avls

--- src/avl.c
+++ src/avl.c
@@ -1,17 +1,37 @@
 #include "avl.h"
+#include <stdlib.h>
 
-typedef enum fatorBalanco {ESQ, EQ, DIR} fatorBalnco; 
+typedef enum fatorBalanco {ESQ, EQ, DIR} FatorBalanco; 
 
 typedef struct nodoAVL {
 	struct nodoAVL * direita;
 	struct nodoAVL * esquerda;
 	ValorNodo valor;
-	fatorBalanco equilibrio;
+	FatorBalanco equilibrio;
 } AVL_NODO;
 	
 typedef struct TCD_AVL {
 	AVL_NODO* raiz;
+	/* funcao de comparacao entre nodos */
 	int (*compara)(const void*, const void*);
 } TCD_AVL;
 
-//lembrete: por a static as funcoes privadas */
+//lembrete: por a static as funcoes privadas 
+
+// ver o que fazer quando falha
+AVL criaAVL(int (*compar)(const void *, const void*)){
+	AVL nova = (AVL) malloc(sizeof(TCD_AVL));
+	if(nova == NULL) return NULL;
+	nova -> raiz = NULL;
+	nova -> compara = compar;
+	return nova;
+}
+
+AVL insere(AVL arvore, ValorNodo val){
+	if(arvore == NULL) return NULL;
+	int comparador(const void *, const void *);
+	
+	comparador = arvore -> compara;
+			
+
+}
--- src/avl.h
+++ src/avl.h
@@ -3,9 +3,9 @@ typedef ValorNodo void*;
 
 /* API para trabalhar com AVL's */
 /* necessario passar funcao de comparacao entre elementos da arvore */
-AVL criaAVL(int (*compar)(const void *, const void*));
+/* nota: se o valor for <0, o 1o argumento é o menor, se for 0 sao iguais, >0 => 2o e o menor*/
+AVL criaAVL(int (*compar)(const ValorNodo, const ValorNodo));
 AVL insere(AVL arvore, ValorNodo val);
 AVL apagaAVL(AVL arvore);
 int existeAVL(AVL arv, ValorNodo val);
 ValorNodo criaValorNodo(void * val);
-
