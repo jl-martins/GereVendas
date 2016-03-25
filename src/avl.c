924b20a6a3cb004c93ebafedb1fe342650c04882
Joao Pereira <joaopereira_19@hotmail.com>
2016-03-25 12:24:16 +0000

    update avl's

--- src/avl.c
+++ src/avl.c
@@ -1,12 +1,16 @@
-enum fatorBalanco {esq, eq, dir};
+#include "avl.h"
+
+typedef enum fatorBalanco {ESQ, EQ, DIR} fatorBalnco; 
 
 typedef struct nodoAVL {
 	struct nodoAVL * direita;
 	struct nodoAVL * esquerda;
-	void * valor;
+	ValorNodo valor;
 	fatorBalanco equilibrio;
 } AVL_NODO;
 	
 typedef struct TCD_AVL {
 	AVL_NODO* raiz;
 } TCD_AVL;
+
+//lembrete: por a static as funcoes privadas */
--- src/avl.h
+++ src/avl.h
@@ -1,1 +1,10 @@
 typedef struct TCD_AVL* AVL;  
+typedef ValorNodo void*;
+
+/* API para trabalhar com AVL's */
+AVL criaAVL();
+AVL insere(AVL arvore, ValorNodo val);
+AVL apagaAVL(AVL arvore);
+int existeAVL(AVL arv, ValorNodo val);
+ValorNodo criaValorNodo(void * val);
+
