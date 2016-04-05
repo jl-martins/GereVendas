dfeffa7ffe813a14d953f3db8f160f8c821994d8
linuxPenguin <a68646@alunos.uminho.pt>
2016-04-05 21:58:39 +0100

    Organizei o módulo avl.c e converti os comentários para a forma standard: /* */

--- src/avl.c
+++ src/avl.c
@@ -5,15 +5,11 @@
 
 #if MODO_TESTE
 	#include <stdio.h>
-	#define comentario1 /
-	#define comentario2 *	
-	#define TESTE_INICIO comentario1##comentario2
-	#define TESTE_FIM comentario2##comentario1
+	#define TO_INT(ValorNodo) *((int *) (ValorNodo))
 #endif
 
-
-//definir funçoes para travessias!! e toString
-//fazer codigo mais seguro para quando malloc falha
+/* definir funçoes para travessias!! e toString
+   fazer codigo mais seguro para quando malloc falha */
 typedef enum fatorBalanco {ESQ, EQ, DIR} FatorBalanco; 
 
 typedef struct nodoAVL {
@@ -25,46 +21,71 @@ typedef struct nodoAVL {
 	
 typedef struct TCD_AVL {
 	AVL_NODO* raiz;
-	// funcao de comparacao entre nodos - se o primeiro < segundo, o valor deve ser negativo 
+	/* funcao de comparacao entre nodos - se o primeiro < segundo, o valor deve ser negativo */
 	int (*compara)(const void*, const void*);
+	/* ValorNodo criaValorNodo(void * val); */
 	int tamanho;
 } TCD_AVL;
 
-//lembrete: por a static as funcoes privadas 
-// Protótipos das funções privadas ao ficheiro 
-static AVL_NODO* rodaDireita(AVL_NODO* t);
-static AVL_NODO* equilibraDireita(AVL_NODO * t);
-static AVL_NODO* insereDireita(AVL_NODO * raiz, ValorNodo val, int (*compara) (const void*, const void*), int * cresceu);
-static AVL_NODO* insereNodo(AVL_NODO * raiz, ValorNodo val, int (*compara) (const void *, const void *), int * cresceu);
-static AVL_NODO* equilibraEsquerda(AVL_NODO *raiz);
-static AVL_NODO* rodaEsquerda(AVL_NODO *raiz);
-static AVL_NODO* equilibraEsquerda(AVL_NODO *raiz);
-static AVL_NODO* insereEsquerda(AVL_NODO *raiz, ValorNodo val, int (*compara) (const void *, const void *), int *cresceu);
-static int alturaAux(const AVL_NODO *raiz);
+/* lembrete: por a static as funcoes privadas */
 
+/* Protótipos das funções privadas ao ficheiro */
+static AVL_NODO* insereNodo(AVL_NODO* raiz, ValorNodo val, int (*compara) (const void *, const void *), int* cresceu);
+static AVL_NODO* insereEsquerda(AVL_NODO* raiz, ValorNodo val, int (*compara) (const void *, const void *), int* cresceu);
+static AVL_NODO* insereDireita(AVL_NODO* raiz, ValorNodo val, int (*compara) (const void *, const void *), int* cresceu);
+static AVL_NODO* equilibraEsquerda(AVL_NODO* raiz);
+static AVL_NODO* equilibraDireita(AVL_NODO* raiz);
+static AVL_NODO* rodaEsquerda(AVL_NODO* raiz);
+static AVL_NODO* rodaDireita(AVL_NODO* raiz);
+static int alturaAux(const AVL_NODO* raiz);
 
 /* DEBUG*/
-TESTE_INICIO
-int compara(const void * x, const void * y){
-	return *(int *)x - *(int *)y;
+/* TESTE_INICIO */
+int compara(const void* x, const void* y){
+	return TO_INT(x) - TO_INT(y);
 }
 
 void printArvore(AVL_NODO* raiz){
 	static int tabs = 0;
 	int i = 0;
 	if(raiz == NULL){
-//		for (i = 0; i < tabs; i++) printf("\t");
+/*		for (i = 0; i < tabs; i++) printf("\t"); */
 		 printf("NULL\n");
 		 tabs--;
 	}else{
 		tabs++;
 		printArvore(raiz->esquerda);
 		
-//		for (i = 0; i < tabs; i++) printf("\t");
+/*		for (i = 0; i < tabs; i++) printf("\t"); */
 		printf("%d\n", *((int*) raiz -> valor));
 		tabs++;
 		printArvore(raiz->direita);
 	}	
+}
+
+/* Imprime uma AVL */
+void imprimeAVL(AVL_NODO* raiz){
+	AVL_NODO *esq, *dir;
+
+	if(!raiz)
+		return;
+
+	esq = raiz->esquerda;
+	dir = raiz->direita;
+
+	if(!esq && !dir)
+		printf("%d\n", TO_INT(raiz->valor));
+	else{
+		if(esq){
+			printf("%d -> (E) %d\n", TO_INT(raiz->valor), TO_INT(esq->valor));
+			imprimeAVL(esq);
+		}
+
+		if(dir){
+			printf("%d -> (D) %d\n", TO_INT(raiz->valor), TO_INT(dir->valor));
+			imprimeAVL(dir);
+		}
+	}
 }
 
 int main(){
@@ -80,62 +101,79 @@ int main(){
 	insere(nova, &d);
 	printf("\n");
 	printArvore(nova->raiz);
-}
-TESTE_FIM
 
-// ver o que fazer quando falha
-AVL criaAVL(int (*compar)(const void*, const void*)){
+	return 0;
+}
+/* TESTE_FIM */
+
+/* ver o que fazer quando falha */
+AVL criaAVL(int (*compar)(const void *, const void *)){
 	AVL nova = (AVL) malloc(sizeof(TCD_AVL));
-	if(nova == NULL) return NULL;
+	
+	if(nova == NULL)
+		return NULL;
 	nova -> raiz = NULL;
 	nova -> compara = compar;
 	nova -> tamanho = 0;
 	return nova;
 }
 
-static AVL_NODO* rodaDireita(AVL_NODO* t){
-	AVL_NODO* aux;
-	if ((! t) || (! t -> esquerda)) {
-		// inserir código de tratamento de erros 
-	} else {
-		aux = t -> esquerda;
-		t -> esquerda = aux -> direita;
-		aux -> direita = t;
-		t = aux;
+/* ver qual deve ser o tipo de retorno */
+AVL insere(AVL arvore, ValorNodo val){
+	int cresceu;
+	
+	if(arvore == NULL){
+		/* Podemos usar IO nos modulos?? 
+		   fprintf(stderr, "Necessário inicializar árvore"); */
+		return NULL;
+	}else{
+		arvore -> tamanho++;		
+		arvore -> raiz = insereNodo(arvore -> raiz, val, arvore -> compara, &cresceu);	
 	}
-	return t;
+	return arvore;
 }
 
-static AVL_NODO* equilibraDireita(AVL_NODO * t){
-	if (t -> direita -> fatorBalanco == DIR) {
-		// Rotacao simples a esquerda
-		t = rodaEsquerda(t);
-		t -> fatorBalanco = EQ;
-		t -> esquerda -> fatorBalanco = EQ;
-	} else {
-		//Dupla rotacao
-		t -> direita = rodaDireita(t -> direita);
-		t = rodaEsquerda(t);
-		switch (t -> fatorBalanco) {
-			case EQ:
-				t -> esquerda -> fatorBalanco = EQ;
-				t -> direita -> fatorBalanco = EQ;
-				break;
+static AVL_NODO* insereNodo(AVL_NODO* raiz, ValorNodo val, int (*compara) (const void *, const void *), int* cresceu){
+	AVL_NODO * ret;
+	
+	if(raiz == NULL){
+		ret = raiz = (AVL_NODO *) malloc(sizeof(AVL_NODO));
+		raiz -> valor = val;	
+		raiz -> esquerda = raiz -> direita = NULL;
+		raiz -> fatorBalanco = EQ;
+		*cresceu = 1;
+	}
+	else if(compara(val, raiz -> valor) < 0) /* raiz->valor > val */
+		ret = insereEsquerda(raiz, val, compara, cresceu);
+	else
+		ret = insereDireita(raiz, val, compara, cresceu);	 
+	return ret;
+}
+
+static AVL_NODO* insereEsquerda(AVL_NODO* raiz, ValorNodo val, int (*compara) (const void *, const void *), int* cresceu){
+	raiz->esquerda = insereNodo(raiz->esquerda, val, compara, cresceu);
+	
+	if(*cresceu){
+		switch(raiz->fatorBalanco){
 			case ESQ:
-				t -> esquerda -> fatorBalanco = EQ;
-				t -> direita -> fatorBalanco = DIR;
+				raiz = equilibraEsquerda(raiz);
+				*cresceu = 0;
+				break;
+			case EQ:
+				raiz->fatorBalanco = ESQ;
 				break;
 			case DIR:
-				t -> esquerda -> fatorBalanco = ESQ;
-				t -> direita -> fatorBalanco = EQ;
+				raiz->fatorBalanco = EQ;
+				*cresceu = 0;
+				break;
 		}
-		t -> fatorBalanco = EQ;
 	}
-	return t;
+	return raiz;
 }
 
-static AVL_NODO* insereDireita(AVL_NODO * raiz, ValorNodo val, int (*compara) (const void*, const void*), int * cresceu){
+static AVL_NODO* insereDireita(AVL_NODO* raiz, ValorNodo val, int (*compara) (const void *, const void *), int* cresceu){
 	raiz -> direita = insereNodo(raiz -> direita, val, compara, cresceu);
+	
 	if(*cresceu){
 		switch(raiz -> fatorBalanco){
 			case ESQ:
@@ -154,46 +192,16 @@ static AVL_NODO* insereDireita(AVL_NODO * raiz, ValorNodo val, int (*compara) (c
 	return raiz;
 }
 
-static AVL_NODO* insereNodo(AVL_NODO * raiz, ValorNodo val, int (*compara) (const void *, const void *), int * cresceu){
-	AVL_NODO * ret;
-	if(raiz == NULL){
-		ret = raiz = (AVL_NODO *) malloc(sizeof(AVL_NODO));
-		raiz -> valor = val;	
-		raiz -> esquerda = raiz -> direita = NULL;
-		raiz -> fatorBalanco = EQ;
-		*cresceu = 1;
-	}
-	else if(compara(val, raiz -> valor) < 0)
-		// se raiz->valor > val
-		ret = insereEsquerda(raiz, val, compara, cresceu);
-	else ret = insereDireita(raiz, val, compara, cresceu);	 
-	return ret;
-}	
-
-// ver qual deve ser o tipo de retorno 
-AVL insere(AVL arvore, ValorNodo val){
-	int cresceu;
-	if(arvore == NULL){
-		// Podemos usar IO nos modulos?? 
-		//fprintf(stderr, "Necessário inicializar árvore");
-		return NULL;
-	}else{
-		arvore -> tamanho++;		
-		arvore -> raiz = insereNodo(arvore -> raiz, val, arvore -> compara, &cresceu);	
-	}
-	return arvore;
-}
-
-static AVL_NODO* equilibraEsquerda(AVL_NODO *raiz){
+static AVL_NODO* equilibraEsquerda(AVL_NODO* raiz){
 
 	if(raiz->esquerda->fatorBalanco == ESQ){
-		// rotação simples à direita
+		/* rotação simples à direita */
 		raiz = rodaDireita(raiz);
 		raiz->fatorBalanco = EQ;
 		raiz->direita->fatorBalanco = EQ;
 	}
 	else{
-		// Dupla rotação
+		/* Dupla rotação */
 		raiz->esquerda = rodaEsquerda(raiz->esquerda);
 		raiz = rodaDireita(raiz);
 
@@ -216,12 +224,39 @@ static AVL_NODO* equilibraEsquerda(AVL_NODO *raiz){
 	return raiz;
 }
 
-// JM
-static AVL_NODO* rodaEsquerda(AVL_NODO *raiz){
+static AVL_NODO* equilibraDireita(AVL_NODO* raiz){
+	if (raiz -> direita -> fatorBalanco == DIR) {
+		/* Rotacao simples a esquerda */
+		raiz = rodaEsquerda(raiz);
+		raiz -> fatorBalanco = EQ;
+		raiz -> esquerda -> fatorBalanco = EQ;
+	} else {
+		/* Dupla rotacao */
+		raiz -> direita = rodaDireita(raiz -> direita);
+		raiz = rodaEsquerda(raiz);
+		switch (raiz -> fatorBalanco) {
+			case EQ:
+				raiz -> esquerda -> fatorBalanco = EQ;
+				raiz -> direita -> fatorBalanco = EQ;
+				break;
+			case ESQ:
+				raiz -> esquerda -> fatorBalanco = EQ;
+				raiz -> direita -> fatorBalanco = DIR;
+				break;
+			case DIR:
+				raiz -> esquerda -> fatorBalanco = ESQ;
+				raiz -> direita -> fatorBalanco = EQ;
+		}
+		raiz -> fatorBalanco = EQ;
+	}
+	return raiz;
+}
+
+static AVL_NODO* rodaEsquerda(AVL_NODO* raiz){
 	AVL_NODO *aux;
 
 	if(!raiz || !raiz->direita){
-		// tratamento de erros 
+		/* tratamento de erros */
 	}
 	else{
 		aux = raiz->direita;
@@ -233,29 +268,59 @@ static AVL_NODO* rodaEsquerda(AVL_NODO *raiz){
 	return raiz;
 }
 
-static AVL_NODO* insereEsquerda(AVL_NODO *raiz, ValorNodo val, int (*compara) (const void *, const void *), int *cresceu){
-	raiz->esquerda = insereNodo(raiz->esquerda, val, compara, cresceu);
+static AVL_NODO* rodaDireita(AVL_NODO* raiz){
+	AVL_NODO* aux;
 	
-	if(*cresceu){
-		switch(raiz->fatorBalanco){
-			case ESQ:
-				raiz = equilibraEsquerda(raiz);
-				*cresceu = 0;
-				break;
-			case EQ:
-				raiz->fatorBalanco = ESQ;
-				break;
-			case DIR:
-				raiz->fatorBalanco = EQ;
-				*cresceu = 0;
-				break;
-		}
+	if ((! raiz) || (! raiz -> esquerda)) {
+		/* inserir código de tratamento de erros */
+	} else {
+		aux = raiz -> esquerda;
+		raiz -> esquerda = aux -> direita;
+		aux -> direita = raiz;
+		raiz = aux;
 	}
 	return raiz;
 }
 
-int altura(const AVL arv){
-	return (arv == NULL) ? -1 : alturaAux(arv->raiz);
+/**
+ * Verifica se um determinado valor existe numa AVL
+ * @param arv Árvore AVL onde o valor será procurado
+ * @param val Valor a procurar
+ * @return 1 se o valor existir; 0 caso contrário.
+ */
+int existeAVL(AVL arv, ValorNodo val){
+	int existe = 0;
+	int r_compara; /* resultado de uma comparação */
+	AVL_NODO *nodo_atual = arv->raiz;
+	
+	while(!existe && nodo_atual){
+		r_compara = arv->compara(nodo_atual->valor, val);
+		
+		if(r_compara < 0) /* procura na subárvore esquerda */
+			nodo_atual = nodo_atual->esquerda;
+		else if(r_compara > 0) /* procura na subárvore direita */
+			nodo_atual = nodo_atual->direita;
+		else
+			existe = 1;
+	}
+
+	return existe;
+}
+
+/**
+ * @param arvore AVL cujo tamanho será retornado
+ * @return Tamanho de 'arvore' se esta existir. -1 caso contrário.
+ */
+int tamanho(const AVL arvore){
+	return (arvore == NULL) ? -1 : arvore->tamanho;
+}
+
+/**
+ * @param arvore AVL cuja altura será calculada
+ * @return A altura de 'arvore' se esta exisitir. -1 caso contrário.
+ */ 
+int altura(const AVL arvore){
+	return (arvore == NULL) ? -1 : alturaAux(arvore->raiz);
 }
 
 static int alturaAux(const AVL_NODO *raiz){
--- src/avl.h
+++ src/avl.h
@@ -1,11 +1,15 @@
 typedef struct TCD_AVL* AVL;  
 typedef void* ValorNodo;
 
-/* API para trabalhar com AVL's */
-/* necessario passar funcao de comparacao entre elementos da arvore */
-/* nota: se o valor for <0, o 1o argumento é o menor, se for 0 sao iguais, >0 => 2o e o menor*/
+/* Cria uma AVL vazia, que vai utilizar a função de comparação 'compar' */
 AVL criaAVL(int (*compar)(const void *,const void *));
+/* Insere um valor na AVL 'arvore' */
 AVL insere(AVL arvore, ValorNodo val);
+/* Testa se o valor 'val' ocorre na AVL 'arvore' */
+int existeAVL(const AVL arvore, ValorNodo val);
+/* Apaga uma AVL (i.e.: liberta toda a memória alocada para a mesma) */
 AVL apagaAVL(AVL arvore);
-int existeAVL(AVL arv, ValorNodo val);
-ValorNodo criaValorNodo(void * val);
+/* Devolve o número de nodos de uma AVL */
+int tamanho(const AVL arvore);
+/* Devolve a altura de uma AVL */
+int altura(const AVL arvore);
