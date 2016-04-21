97194dd05536b3b8a7985a5fa0da1148d5619793
linuxPenguin <a68646@alunos.uminho.pt>
2016-04-21 15:30:23 +0100

    Eliminei a macro NUM_CAMPOS do venda.h porque não estava a ser utilizada

--- src/venda.h
+++ src/venda.h
@@ -1,9 +1,6 @@
 #ifndef VENDA_H
 #define VENDA_H
 
-/* Definição dos campos nas linhas do ficheiro de vendas. Estes valores devem ser
-   sempre alterados em simultâneo de forma a manterem concordância */
-#define NUM_CAMPOS 7
 #define N_TIPOS_VENDA 2
 
 /* Definição de tipos enumerados relacionados com as vendas */
