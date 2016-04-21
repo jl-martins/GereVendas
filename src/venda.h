8aa6833c4e0da20f38dda03289f2362068c2185d
linuxPenguin <a68646@alunos.uminho.pt>
2016-04-21 15:32:33 +0100

    Removi um enum de venda.h que não estava a ser utilizado

--- src/venda.h
+++ src/venda.h
@@ -3,8 +3,7 @@
 
 #define N_TIPOS_VENDA 2
 
-/* Definição de tipos enumerados relacionados com as vendas */
-enum campoVenda {CODIGO_PROD = 0, PRECO, UNIDADES, TIPO_COMPRA, CODIGO_CLIENTE, MES, FILIAL};
+/* Enumeração dos tipos de venda */
 typedef enum tipoVenda{N = 0, P = 1} TipoVenda;
 
 #endif
