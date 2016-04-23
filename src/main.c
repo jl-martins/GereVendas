adecb6930a4117af576ccef9eac4c9da4c1aa173
linuxPenguin <a68646@alunos.uminho.pt>
2016-04-23 01:14:35 +0100

    Adicionei uma pequena melhoria no ciclo for da query11

--- src/main.c
+++ src/main.c
@@ -1093,9 +1093,8 @@ static int query11()
 		if(top3){
 			int i;
 			printf("Códigos dos 3 produtos em que o cliente %s gastou mais dinheiro\n", codigoCliente);
-			for(i = 0; i < 3; ++i)
-				if(top3[i] != NULL)
-					printf("%dº: %s\n", i+1, top3[i]);
+			for(i = 0; top3[i] && i < 3; ++i)
+				printf("%dº: %s\n", i+1, top3[i]);
 			apagaArray((void **) top3, 3, free);
 			ENTER_PARA_CONTINUAR();
 		}
