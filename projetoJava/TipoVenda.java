7b3dbfcfa0c7af786c5025aa1557d4691fb67ba3
Joao Pereira <joaopereira_19@hotmail.com>
2016-05-05 22:01:18 +0100

    update no TipoVenda, adicionada exceçao para tipos invalidos e funçao que converte um char num tipo de venda

--- projetoJava/TipoVenda.java
+++ projetoJava/TipoVenda.java
@@ -6,5 +6,15 @@
  * @version (version number or date here)
  */
 public enum TipoVenda {
-    P, N
+    P, N;
+    
+    public TipoVenda tipoFromChar(char c) throws TipoVendaInvalidoException {
+        switch(c){
+            case 'P': return P;
+            case 'N': return N;
+            default : throw new TipoVendaInvalidoException("Tipo de Venda Invalido");
+        }            
+    }
+    
+    /* fazer para ler de strings */
 }
