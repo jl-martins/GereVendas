716a0bf48b28522381d0cb06af0d5cc6b9b29ea0
Manel18 <peaceandhendrix@gmail.com>
2016-05-26 17:47:44 +0100

    Tarefa5 e fromString no TipoVenda

--- projetoJava/TipoVenda.java
+++ projetoJava/TipoVenda.java
@@ -8,13 +8,25 @@
 public enum TipoVenda {
     P, N;
     
-    public TipoVenda tipoFromChar(char c) throws TipoVendaInvalidoException {
+    public static TipoVenda tipoFromChar(char c) throws TipoVendaInvalidoException {
         switch(c){
             case 'P': return P;
             case 'N': return N;
             default : throw new TipoVendaInvalidoException("Tipo de Venda Invalido");
         }            
     }
-    
+
+	public static TipoVenda tipoFromString(String str) throws TipoVendaInvalidoException
+	{
+		/*Mais Exceptions!?*/
+		TipoVenda tipo = null;
+		String aux = null;
+		try{
+			aux = str.trim().toUpperCase();	
+		    if(aux.length()>1) throw new TipoVendaInvalidoException("");
+			tipo = tipoFromChar(aux.charAt(0));
+		}catch(IllegalArgumentException | NullPointerException | TipoVendaInvalidoException  e){throw new TipoVendaInvalidoException("O tipo de apartamento '" + str + "' é inválido!");}
+		return tipo;
+	}
     /* fazer para ler de strings */
 }
